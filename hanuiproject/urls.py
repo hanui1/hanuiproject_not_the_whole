cadaec39a61428b42fe1d41555d8d458bb00f91e
hanui <bbyong_1@likelion.org>
2019-03-04 03:16:43 +0900

    url 3

--- hanuiproject/urls.py
+++ hanuiproject/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 import blog.views
 import accounte.views
-from django.conf import settings
+# from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 # django.contrib.auth.views.LoginView
